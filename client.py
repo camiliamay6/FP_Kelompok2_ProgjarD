51f460595a49eb6a57c4613c07b0c6fae2bbebbd
17-193VaniaC <17-193VaniaC@users.noreply.github.com>
2020-05-13 22:28:19 +0700

    test4
    UI jadi, tapi masih butut

--- client.py
+++ client.py
@@ -18,8 +18,8 @@ class Window(Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        
-        for F in (CreateRoom_frame, JoinRoom_frame, Main_Menu):
+        #setiap kelas frame masukin ke dalam kurung sini ya
+        for F in (CreateRoom_frame, JoinRoom_frame, Main_Menu, EnterUserName_frame, PlayMode_frame):
             frame = F(container, self)
             self.frames[F]=frame
             frame.grid(row=0, column=0, sticky="nsew")
@@ -40,31 +40,53 @@ class Main_Menu(Frame):
         b_new_room.pack()
             
         b_join_room = Button(self, text="Join room", command=lambda: controller.show_frame(JoinRoom_frame))
-        b_join_room.pack()        
-    #Buat room baru
+        b_join_room.pack()   
+        
+#Buat room baru
 class CreateRoom_frame(Frame):
      def __init__(self, parent, controller):
         Frame.__init__(self, parent)
-        label = Label(self, text="Main Menu")
+        b_mainmenu = Button(self, text="Back", command=lambda: controller.show_frame(Main_Menu))
+        b_mainmenu.pack(pady=15, padx=15)
+        label = Label(self, text="Berikut id Room anda")
         label.pack(pady=10,padx=10)
-            
-        b_new_room = Button(self, text="Create New Room", command=lambda: controller.show_frame(CreateRoom_frame))
-        b_new_room.pack()
-            
-        b_join_room = Button(self, text="Join room", command=lambda: controller.show_frame(JoinRoom_frame))
-        b_join_room.pack()
+        #harus dapet nomor roomnya, sementara asal dulu ya
+        Room_number= Label(self, text="78h8bg")
+        Room_number.pack(pady=15,padx=15)        
+        b_enter = Button(self, text="Next", command=lambda: controller.show_frame(EnterUserName_frame))
+        b_enter.pack(pady=15, padx=15)
+        
 #Menu Join
 class JoinRoom_frame(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
-        label = Label(self, text="Main Menu")
+        b_mainmenu = Button(self, text="Back", command=lambda: controller.show_frame(Main_Menu))
+        b_mainmenu.pack(pady=15, padx=15)
+        label = Label(self, text="Masukan id Ruangan")
         label.pack(pady=10,padx=10)
-        
-        b_new_room = Button(self, text="Create New Room", command=lambda: controller.show_frame(CreateRoom_frame))
-        b_new_room.pack()
-            
-        b_join_room = Button(self, text="Join room", command=lambda: controller.show_frame(JoinRoom_frame))
+        room_number_input=Entry(self, text="masukan id ruangan", bd=5)
+        room_number_input.pack()
+        b_join_room = Button(self, text="Next", command=lambda: controller.show_frame(EnterUserName_frame))
         b_join_room.pack()
+        
+#Menu masuan Username
+class EnterUserName_frame(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        b_mainmenu = Button(self, text="Back", command=lambda: controller.show_frame(Main_Menu))
+        b_mainmenu.pack(pady=15, padx=15)
+        label = Label(self, text="Masukan Username")
+        label.pack(pady=10,padx=10)
+        username_input=Entry(self, text="masukan username", bd=5)
+        username_input.pack()
+        b_start = Button(self, text="Enter", command=lambda: controller.show_frame(PlayMode_frame))
+        b_start.pack()
+
+class PlayMode_frame(Frame):
+     def __init__(self, parent, controller):
+         Frame.__init__(self, parent)
+         label = Label(self, text="Ini chat room")
+         label.pack(pady=10,padx=10)
         
         
 app = Window()
