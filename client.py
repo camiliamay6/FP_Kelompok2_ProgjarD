aaf90bf8bd65d7c9c1c03c155b843e3a849578e5
Shanssea <Shanssea@users.noreply.github.com>
2020-05-14 12:14:44 +0700

    nambahin code generator untuk create room

--- client.py
+++ client.py
@@ -2,9 +2,12 @@ import socket
 import select
 import sys
 import msvcrt
+import string
+import random
 #FTP
 from ftplib import FTP
 from tkinter import *
+from tkinter.ttk import *
 
 #konfigurasi dengan server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -28,11 +31,12 @@ class Window(Tk):
 
         self.frames = {}
         #setiap kelas frame masukin ke dalam kurung sini ya
-        for F in (CreateRoom_frame, JoinRoom_frame, Main_Menu, EnterUserName_frame, PlayMode_frame):
+        for F in (CreateRoom_frame, JoinRoom_frame, Main_Menu, EnterUserName_frame, NotFound_frame, PlayMode_frame):
             frame = F(container, self)
             self.frames[F]=frame
             frame.grid(row=0, column=0, sticky="nsew")
         self.show_frame(Main_Menu)
+
     #show frame    
     def show_frame(self, cont):
         frame = self.frames[cont]
@@ -52,7 +56,19 @@ class Window(Tk):
             frame = self.frames[EnterUserName_frame]
             frame.tkraise()
         else:
-            print("room tidak ditemukan")
+            frame = self.frames[NotFound_frame]
+            frame.tkraise()
+
+    # generate random string
+    def randomKey(self, entry):
+        entry.delete(0, END)
+        lettersAndDigits = string.ascii_letters + string.digits
+        return ''.join((random.choice(lettersAndDigits) for i in range(5)))
+
+    # masukin kode
+    def generateKode(self, entry): 
+        password1 = self.randomKey(entry) 
+        entry.insert(10, password1) 
         
 #Buat halaman Main Menu
 class Main_Menu(Frame):
@@ -69,17 +85,20 @@ class Main_Menu(Frame):
         
 #Buat room baru
 class CreateRoom_frame(Frame):
-     def __init__(self, parent, controller):
+    def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         b_mainmenu = Button(self, text="Back", command=lambda: controller.show_frame(Main_Menu))
         b_mainmenu.pack(pady=15, padx=15)
         
         label = Label(self, text="Berikut id Room anda")
         label.pack(pady=10,padx=10)
-        
         #harus dapet nomor roomnya, sementara asal dulu ya
-        Room_number= Label(self, text="78h8bg")
-        Room_number.pack(pady=15,padx=15)        
+        Room_number = Entry(self)
+        Room_number.pack(pady=15,padx=15)   
+
+        b_generate = Button(self, text="Generate", command=lambda: controller.generateKode(Room_number))
+        b_generate.pack(pady=15, padx=15)   
+
         b_enter = Button(self, text="Next", command=lambda: controller.show_frame(EnterUserName_frame))
         b_enter.pack(pady=15, padx=15)
         
@@ -93,7 +112,7 @@ class JoinRoom_frame(Frame):
         label = Label(self, text="Masukan id Ruangan")
         label.pack(pady=10,padx=10)
         
-        room_number_input=Entry(self, bd=5)
+        room_number_input=Entry(self)
         room_number_input.pack()
         
         b_join_room = Button(self, text="Next", command=lambda: controller.Join_msg(room_number_input.get()))
@@ -110,10 +129,16 @@ class EnterUserName_frame(Frame):
         b_mainmenu.pack(pady=15, padx=15)
         label = Label(self, text="Masukan Username")
         label.pack(pady=10,padx=10)
-        username_input=Entry(self, text="masukan username", bd=5)
+        username_input=Entry(self, text="masukan username")
         username_input.pack()
         b_start = Button(self, text="Enter", command=lambda: controller.show_frame(PlayMode_frame))
         b_start.pack()
+
+class NotFound_frame(Frame):
+    def __init__(self,parent, controller):
+        Frame.__init__(self,parent)
+        label = Label(self, text="Room Tidak Ditemukan :(")
+        label.pack(pady=20,padx=20)
 
 class PlayMode_frame(Frame):
      def __init__(self, parent, controller):
